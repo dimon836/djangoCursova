86f839224c82d723ad706883a3f47b07bb8afae7
dimon836 <dg1472534@gmail.com>
2020-12-27 17:15:20 +0200

    add class Coupon to models.py

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import CompanyDB
+from .models import Coupon
 
 
 admin.site.register(CompanyDB)
+admin.site.register(Coupon)
--- main/models.py
+++ main/models.py
@@ -5,11 +5,12 @@ class CompanyDB(models.Model):
     title = models.CharField('Name', max_length=40)
     task = models.TextField('Description')
 
-    def __str__(self):
-        return self.title
-
 
 class Coupon(models.Model):
     type_of_transport = models.CharField('TypeOfTransport', max_length=64)
     cost_road = models.IntegerField('RoadCost')
     cost_housing = models.IntegerField('AccommodationCost')
+
+    class Meta:
+        verbose_name = 'Купон'
+        verbose_name_plural = 'Купоны'
